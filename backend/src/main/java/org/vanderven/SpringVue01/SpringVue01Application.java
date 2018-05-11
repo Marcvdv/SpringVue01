ae1025b01dec0a48fe50082d94929282283f2187
marcvdv <marccvdv@gmail.com>
2018-05-11 10:51:22 +0200

    First commit

---
+++ backend/src/main/java/org/vanderven/SpringVue01/SpringVue01Application.java
@@ -0,0 +1,12 @@
+package org.vanderven.SpringVue01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVue01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVue01Application.class, args);
+	}
+}
